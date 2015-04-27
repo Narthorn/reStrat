24672ca79a879f7be4c486b09e1d2c60c6ef7a89
chillmastor <chillmastor@hotmail.de>
2015-04-28 00:34:36 +0200

    reworked ohmna profile

--- profiles/Genetic Archives/ohmna.lua
+++ profiles/Genetic Archives/ohmna.lua
@@ -8,94 +8,147 @@
 
 --Fight initiation function
 function ReStrat:ohmnaInit(unit)
-	
+	nextgenerator = "highest"
+	eruptlistener = false
 	local ohmna = "Dreadphage Ohmna";
 	if unit ~= nil then ReStrat:trackHealth(unit, ReStrat.color.red) end
-	--Create CD timer for bored
-	--local boredCD = function() ReStrat:createAlert("Bored Cooldown!", 45, nil, ReStrat.color.purple, nil) end
-
+	firsttentacle = false
+	lastphasestarted = false
+	
+	--Boredom
+	if self:IsActivated("Dreadphage Ohmna", "Boredom (Tank Switch)") then
+		local boredCD = function()
+			ReStrat:createPop("Boredom!")
+			ReStrat:createAlert("Bored Cooldown!", 42, nil, ReStrat.color.purple, nil)
+		end
+		ReStrat:OnDatachron("Dreadphage Ohmna is bored with", boredCD)
+		ReStrat:createAlert("Bored Cooldown!", 45, nil, ReStrat.color.purple, nil)
+	end
+	
 	--Devour
-	local devourCD = function()
-		ReStrat:createAlert("Devour Cooldown", 20, nil, ReStrat.color.orange, nil)
-	end
-	
-	local devourpop = function()
-		ReStrat:createPop("Devour!", nil)
-		ReStrat:Sound("Sound\\devour.wav")
-	end
-	
-	ReStrat:createCastAlert(ohmna, "Devour", nil, "Icon_SkillMedic_devastatorprobes2", ReStrat.color.red, nil);
-	
-	
-	--AND WELCOME TO THE JAM
-	local slamTimer = function()
-		if not ReStrat.tEncounterVariables.slam then ReStrat.tEncounterVariables.slam = 0 end --Create the slam
-		
-		ReStrat.tEncounterVariables.slam = ReStrat.tEncounterVariables.slam+1;
-		
-		if ReStrat.tEncounterVariables.slam == 3 then
-			ReStrat.tEncounterVariables.slam = 0; -- Clear our slam
-			
-			ReStrat:createPop("Spew Incoming!", nil);
-			ReStrat:Sound("Sound\\spew.wav")
-		
+	if self:IsActivated("Dreadphage Ohmna", "Devour") then
+		local devourpop = function()
+			if not lastphasestarted then 
+				ReStrat:createPop("Devour!", nil)
+				ReStrat:Sound("Sound\\devour.wav")
+			end
 		end
-	end
-	
-	--COME ON AND SLAM
-	ReStrat:onPlayerHit("Body Slam", ohmna, nil, slamTimer)
-	
-	
-	--Torrent notification logic
-	local torrentNotification = function()
-		if not ReStrat.tEncounterVariables.slam or ReStrat.tEncounterVariables.slam == 1 then
-			ReStrat:createAlert("Genetic Torrent Incoming!", 12, nil, ReStrat.color.purple, nil);
-		end
-	end
-	
-	--Create torrent notification
-	ReStrat:createCastTrigger(ohmna, "Body Slam", torrentNotification)
+		ReStrat:OnDatachron("Dreadphage Ohmna hungers....", devourpop)
+		ReStrat:createCastAlert(ohmna, "Devour", nil, "Icon_SkillMedic_devastatorprobes2", ReStrat.color.red, nil)
+	end	
 	
 	--Genetic Torrent 
 	local spewcount = function()
-		if not ReStrat.tEncounterVariables.torrent then ReStrat.tEncounterVariables.torrent = 0 end
-		ReStrat.tEncounterVariables.torrent = ReStrat.tEncounterVariables.torrent+1
-		if ReStrat.tEncounterVariables.torrent == 2 and unit:GetHealth() > 6000000 then
-			ReStrat:createPop("Add phase after Spew!", nil)
-			ReStrat.tEncounterVariables.torrent = 0
+		if not lastphasestarted then
+			if not ReStrat.tEncounterVariables.torrent then ReStrat.tEncounterVariables.torrent = 0 end
+			ReStrat.tEncounterVariables.torrent = ReStrat.tEncounterVariables.torrent+1
+			if ReStrat.tEncounterVariables.torrent == 2 and unit:GetHealth() > 6000000 then
+				ReStrat:createPop("Add phase after Spew!", nil)
+				ReStrat.tEncounterVariables.torrent = 0
+			else
+				ReStrat:createAlert("Next Spew", 80, nil, ReStrat.color.orange, nil)
+			end
 		end
-		
 	end
-	ReStrat:createCastAlert(ohmna, "Genetic Torrent", nil, "Icon_SkillMedic_devastatorprobes2", ReStrat.color.red, spewcount);
+	ReStrat:createCastAlert(ohmna, "Genetic Torrent", nil, "Icon_SkillMedic_devastatorprobes2", ReStrat.color.red, spewcount)
+	if self:IsActivated("Dreadphage Ohmna", "Genetic Torrent") then
+		ReStrat:createAlert("First Spew", 27, nil, ReStrat.color.orange, nil)
+	end
+	
+	--Generator
+	local generatorCD = function()
+		if nextgenerator == "lowest" then
+			ReStrat:createAlert("Next Generator (lowest HP)", 25, nil, ReStrat.color.yellow, nil)
+			nextgenerator = "highest"
+		else
+			ReStrat:createAlert("Next Generator (highest HP)", 25, nil, ReStrat.color.yellow, nil)
+			nextgenerator = "lowest"
+		end
+	end
+	ReStrat:createAlert("First Generator", 25, nil, ReStrat.color.yellow, nil)
+	ReStrat:OnDatachron("A plasma leech begins draining the", generatorCD)
+	
+	--addphase handler
+	local aftererrupt = function()
+		if eruptlistener then
+			if self:IsActivated("Dreadphage Ohmna", "Boredom (Tank Switch)") then
+				ReStrat:createAlert("Bored Cooldown!", 45, nil, ReStrat.color.purple, nil)
+			end
+			if nextgenerator == "lowest" then
+				ReStrat:createAlert("Next Generator (lowest HP)", 32, nil, ReStrat.color.yellow, nil)
+			else
+				ReStrat:createAlert("Next Generator (highest HP)", 32, nil, ReStrat.color.yellow, nil)
+			end
+		end
+		eruptlistener = false
+	end
+	
+	local addphase = function()
+		eruptlistener = true
+		ReStrat:destroyAllAlerts()
+		ReStrat:createCastTrigger("Dreadphage Ohmna", "Erupt", aftererrupt)
+	end
+	ReStrat:OnDatachron("Dreadphage Ohmna submerges into the Bi", addphase)
+	
+	--tentaclespawn handler
+	local nextspawnsoon = function()
+		firsttentacle = false
+	end
+	local tentaclespawn = function()
+		if firsttentacle == false then
+			ReStrat:createAlert("Tentacles", 19, nil, ReStrat.color.blue, nextspawnsoon)
+		end	
+		firsttentacle = true
+	end
+	if self:IsActivated("Dreadphage Ohmna", "Tentacle Spawns") then
+		ReStrat:createUnitTrigger("Tentacle of Ohmna", tentaclespawn)
+	end
 	
 	
-	ReStrat:OnDatachron("Dreadphage Ohmna hungers....", devourpop)
-	--Bored
-	--ReStrat:onPlayerHit("Ravage", ohmna, nil, boredCD)
+	--3worm mechanics
+	local lastphasespew = function()
+		ReStrat:createAlert("Spew!", 17, nil, ReStrat.color.red, nil)
+		ReStrat:createPop("Spew!", nil)
+		ReStrat:Sound("Sound\\spew.wav")
+	end
+	local smalldevour = function(caster)
+		if ReStrat:dist2unit(GameLib.GetPlayerUnit(), caster) < 23 then
+			ReStrat:createPop("Devour!", nil)
+			ReStrat:Sound("Sound\\devour.wav")
+			ReStrat:createAlert("Devour!", 5, nil, ReStrat.color.red, nil)
+		end
+	end
+	local lastphase = function()
+		ReStrat:destroyAllAlerts()
+		lastphasestarted = true
+		if self:IsActivated("Dreadphage Ohmna", "Genetic Torrent") then
+			ReStrat:createCastTrigger(ohmna, "Genetic Torrent", lastphasespew)
+		end
+		ReStrat:createCastTrigger("Ravenous Maw of the Dreadphage", "Devour", smalldevour)
+	end
 	
-	
-	
+	ReStrat:OnDatachron("The Archives quake with the furious might of the Dreadphage", lastphase)
 end
 
 function ReStrat:ohmnanorth(tEventObj)
-	--if self:IsActivated("Maelstrom Authority", "Track Weather Cycle [Event]") then
+	if self:IsActivated("Dreadphage Ohmna", "Track Generators [Event]") then
 		ReStrat:trackEvent(tEventObj, self.color.yellow, "North Generator")
-	--end	
+	end	
 end
 function ReStrat:ohmnaeast(tEventObj)
-	--if self:IsActivated("Maelstrom Authority", "Track Weather Cycle [Event]") then
+	if self:IsActivated("Dreadphage Ohmna", "Track Generators [Event]") then
 		ReStrat:trackEvent(tEventObj, self.color.yellow, "East Generator")
-	--end	
+	end	
 end
 function ReStrat:ohmnasouth(tEventObj)
-	--if self:IsActivated("Maelstrom Authority", "Track Weather Cycle [Event]") then
+	if self:IsActivated("Dreadphage Ohmna", "Track Generators [Event]") then
 		ReStrat:trackEvent(tEventObj, self.color.yellow, "South Generator")
-	--end	
+	end	
 end
 function ReStrat:ohmnawest(tEventObj)
-	--if self:IsActivated("Maelstrom Authority", "Track Weather Cycle [Event]") then
+	if self:IsActivated("Dreadphage Ohmna", "Track Generators [Event]") then
 		ReStrat:trackEvent(tEventObj, self.color.yellow, "West Generator")
-	--end	
+	end	
 end
 -----------------------------------------------------------------------------
 --Encounter Packaging
@@ -120,6 +173,14 @@ ReStrat.tEncounters["Dreadphage Ohmna"] = {
 		},
 		["Track Generators [Event]"] = {
 			strLabel = "Track Generators [Event]",
+			bEnabled = true,
+		},
+		["Boredom (Tank Switch)"] = {
+			strLabel = "Boredom (Tank Switch)",
+			bEnabled = false,
+		},
+		["Tentacle Spawns"] = {
+			strLabel = "Tentacle Spawns",
 			bEnabled = true,
 		},
 	},
