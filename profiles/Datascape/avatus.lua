4cfae2ba533ee6b64e9ee26b1fbfc63281baa8d3
chillmastor <chillmastor@hotmail.de>
2015-04-28 00:34:52 +0200

    imported avatus profile from vim

--- profiles/Datascape/avatus.lua
+++ profiles/Datascape/avatus.lua
@@ -5,21 +5,58 @@
 function ReStrat:avatusInit(unit) -- also lattice for some reason
 	if not ReStrat.tEncounterVariables.firstPhase then
 		ReStrat.tEncounterVariables.firstPhase = true
-		ReStrat:createAlert("Gun Grid", 46, "Icon_SkillEngineer_Target_Acquistion", ReStrat.color.red)
+
+		ReStrat:createAlert("Gun Grid", 20, "Icon_SkillEngineer_Target_Acquistion", ReStrat.color.red)
 		
 		ReStrat:OnDatachron("SECURITY PROTOCOL: Gun Grid Activated.", function()
 			ReStrat:destroyAlert("Gun Grid")
 			ReStrat:createAlert("Obliteration Beam", 45, nil, ReStrat.color.blue, function()
-				ReStrat:createAlert("Obliteration Beam", 45, nil, ReStrat.color.blue)
+				ReStrat:createAlert("Obliteration Beam", 40, nil, ReStrat.color.blue)
 			end)
-			ReStrat:createAlert("Holo Hands", 22, nil, ReStrat.color.green, nil)
+			ReStrat:createAlert("Holo Hands/Guns", 22, nil, ReStrat.color.green, nil)
 			ReStrat:createAlert("Gun Grid", 112, "Icon_SkillEngineer_Target_Acquistion", ReStrat.color.red, nil)
 		end)
 		
 		ReStrat:OnDatachron("Avatus' power begins to surge! Portals have opened!", function()
+			ReStrat:destroyAlert("Obliteration Beam")
 			ReStrat:destroyAlert("Gun Grid")
-			ReStrat:destroyAlert("Holo Hands")
+			ReStrat:destroyAlert("Holo Hands/Guns")
 		end)
+		
+		ReStrat:OnDatachron("The Caretaker has returned!", function()
+			ReStrat:destroyAlert("Obliteration Beam")
+			ReStrat:destroyAlert("Gun Grid")
+			ReStrat:destroyAlert("Holo Hands/Guns")
+			ReStrat:createAlert("Cannons", 8, nil, ReStrat.color.yellow, function()
+				ReStrat:repeatAlert({strLabel = "Next cannons", fRepeat = 23, strColor = ReStrat.color.yellow})
+			end)
+		end)
+	end
+end
+
+function ReStrat:yellowInit()
+	ReStrat:createCastTrigger("Mobius Physics Constructor", "Data Flare", function()
+		ReStrat:createPop("Flare !", nil)
+		ReStrat:Sound("Sound\\quack.wav")
+	end)
+end
+
+function ReStrat:blueInit(unit)
+	if unit:GetDispositionTo(GameLib.GetPlayerUnit()) == 0 then -- portal to blue room has same name
+		local playerColor = ReStrat.tShortcutBars[7][1].spell:GetName():match("%w+") -- only the first word of "Green/Blue/Red Matrix Key"
+	
+		local function Matrix(color)
+			if color == playerColor then
+				ReStrat:createPop(color .. " Disruption Matrix !")
+				ReStrat:Sound("Sound\\quack.wav")
+			end
+		end
+
+		ReStrat:createAuraAlert("Infinite Logic Loop", "Blue Disruption Matrix", 0, nil, function() Matrix("Blue") end) 
+		ReStrat:createAuraAlert("Infinite Logic Loop", "Green Reconstitution Matrix", 0, nil, function() Matrix("Green") end) 
+		ReStrat:createAuraAlert("Infinite Logic Loop", "Red Empowerment Matrix", 0, nil, function() Matrix("Red") end) 
+	
+		ReStrat:createPinFromAura("Disruption Matrix Distortion")
 	end
 end
 
@@ -64,6 +101,8 @@ function ReStrat:latticeInit()
 	ReStrat:createAuraAlert(nil, "Mark of Enmity", nil, "Icon_SkillEngineer_Code_Red", nil)
 end
 
+
+
 function ReStrat:devourerInit(unit)
 	if self:IsActivated("Avatus", "Lines to Devourers") then
 		DrawLib:UnitLine(GameLib.GetPlayerUnit(), unit, ReStrat.color.orange)
@@ -91,11 +130,4 @@ ReStrat.tEncounters["Avatus"] = {
 			bEnabled = true,
 		},
 	},
-}
-
-ReStrat.tEncounters["Holo Hand"] = {
-	startFunction = avatusInit,
-	strCategory = "Not Important",
-	trackHealth = ReStrat.color.blue,
-	tModules = {},
 }
