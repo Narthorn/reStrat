3b72f0c8db3c34f56b74af285b190f6a11855c96
chillmastor <chillmastor@hotmail.de>
2015-04-29 12:49:16 +0200

    canimid updated

--- profiles/Datascape/minis.lua
+++ profiles/Datascape/minis.lua
@@ -5,20 +5,28 @@
 -----------------------------------------------------------------------------
 --Encounter Logic
 -----------------------------------------------------------------------------
-
+--("Warmonger Agratha", "Warmonger Talarii", "Grand Warmonger Tar'gresh")
+--Frostbringer Warlock
 function ReStrat:canimidInit(unit)
 
 	if ReStrat:IsActivated("Fully-Optimized Canimid", "Show People with Debuff") then
-		ReStrat:createPinFromAura("Weak in the Knees") -- add the undermine debuff 
+
+		ReStrat:createPinFromAura("Weak in the Knees", nil, false, "CRB_Interface12_BO")
 		ReStrat:createAuraAlert(GameLib.GetPlayerUnit():GetName(), "Weak in the Knees", nil, "Icon_SkillMedic_urgency", nil)
 		
 	end
 end
 
+function ReStrat:warlockInit(unit)
+	ReStrat:createAlert("Frost Waves", 30, nil, ReStrat.color.orange, nil)
+end
+
+
 
 -----------------------------------------------------------------------------
 --Encounter Packaging
 -----------------------------------------------------------------------------
+
 
 ReStrat.tEncounters["Fully-Optimized Canimid"] = {
 	strCategory  = "Datascape",
