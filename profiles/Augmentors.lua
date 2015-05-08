663dba05a390f053dd7d9436ef915cf3b1f83ff7
Narthorn <narthorn@gmail.com>
2015-05-08 15:57:30 +0200

    Augmentors: Updated profile.

--- profiles/Augmentors.lua
+++ profiles/Augmentors.lua
@@ -1,30 +1,47 @@
 -----------------------------------------------------------------------------
 -- Augmentors - Vim
 
+local tBars = {}
+
 local function augmentorsInit(unit)
+
+	-- Cache health bars for color change
+	tBars[unit:GetId()] = ReStrat.tHealth[unit:GetId()].bar:FindChild("progressBar")
+
 	if not ReStrat.tEncounterVariables.bStarted then
 		ReStrat:OnDatachron("ENGAGING TECHNOPHAGE TRASMISSION", function() -- sic
 			ReStrat:createAlert("Expulsion", 12, nil, ReStrat.color.red)
 		end)
 		
 		--ReStrat:repeatAlert({strLabel = "Radiation Bath", fDelay = 15, fRepeat = 30, strColor = ReStrat.color.green})
-		ReStrat:createAuraAlert(nil, "Compromised Circuitry", 0, nil, function(unit) 
-			for id,tHealth in pairs(ReStrat.tHealth) do
-				tHealth.bar:FindChild("progressBar"):SetBarColor(tHealth.unit == unit and ReStrat.color.purple or ReStrat.color.orange)
-			end
-		end)
+		
+		local function Corrupted(unit) 
+			tBars[unit:GetId()]:SetBarColor(ReStrat.color.purple)
+		end
+		
+		local function Uncorrupted(unit)
+			tBars[unit:GetId()]:SetBarColor(ReStrat.color.orange)
+		end
+		
+		ReStrat:createAuraTrigger(nil, "Compromised Circuitry", Corrupted, Uncorrupted)			
 		
 		ReStrat:createPinFromAura("Strain Incubation")
-		
+				
+		-- Interrupt timer for hardmode laser
+		ReStrat:repeatAlert({strLabel = "Laser Interrupt", fDelay = 8, fRepeat = 14, strColor = ReStrat.color.yellow, fCallback = function()
+			ReStrat:createPop("Interrupt !")
+			Sound.Play(Sound.PlayUIQueuePopsPvP)
+		end})
+		 
 		ReStrat.tEncounterVariables.bStarted = true
 	end
 	
 	local pos = unit:GetPosition()
 	local name = pos.z < 875 and "North" or (pos.x < 1268 and "West" or "East")
-	ReStrat:trackHealth(unit, ReStrat.color.purple, name)
+	ReStrat:trackHealth(unit, ReStrat.color[name == "North" and "purple" or "orange"], name)
 	
-	local function push() 
-		ReStrat:createPop("PULL SOON !")
+	local function push()
+		ReStrat:createPop("PUSH SOON !")
 		Sound.PlayFile("Sound\\quack.wav")
 	end
 	
@@ -33,22 +50,5 @@ local function augmentorsInit(unit)
 	ReStrat:onHealth(unit, 22, push)
 end
 
-local function hardmodeInit()
-	if not ReStrat.tEncounterVariables.bHardMode then
-	 	-- Interrupt timer for hardmode laser
-			
-		ReStrat:repeatAlert({strLabel = "Laser Interrupt", fDelay = 10, fRepeat = 30, strColor = ReStrat.color.yellow, fCallback = function()
-			ReStrat:createPop("Interrupt !")
-			Sound.Play(Sound.PlayUIQueuePopsPvP)
-		end})
-		ReStrat.tEncounterVariables.bHardMode = true
-	end
-end
-
-ReStrat.tEncounters["Prime Phage Distributor"] = {
-	fInitFunction = augmentorsInit,
-}
-
-ReStrat.tEncounters["Prime Evolutionary Operant"] = {
-	fInitFunction = augmentorsInit,
-}
+ReStrat.tEncounters["Prime Phage Distributor"]    = { fInitFunction = augmentorsInit }
+ReStrat.tEncounters["Prime Evolutionary Operant"] = { fInitFunction = augmentorsInit }
