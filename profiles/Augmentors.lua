11d64b156b733309973720ed70d260085a6e419d
Narthorn <narthorn@gmail.com>
2015-05-08 20:53:06 +0200

    Augmentors: Added safezones.

--- profiles/Augmentors.lua
+++ profiles/Augmentors.lua
@@ -1,54 +1,96 @@
 -----------------------------------------------------------------------------
 -- Augmentors - Vim
 
-local tBars = {}
+local tUnits = {}
+local tSafeZones = {
+	North = {
+		{{vPos = Vector3.New(1293.52,-800.50,823.85)},{vPos = Vector3.New(1279.17,-800.50,823.85)}, {vPos = Vector3.New(1286.34,-800.50,811.31)}},
+		{{vPos = Vector3.New(1275.14,-800.50,805.10)},{vPos = Vector3.New(1268.16,-800.50,817.68)}, {vPos = Vector3.New(1260.89,-800.50,805.10)}},
+		{{vPos = Vector3.New(1249.67,-800.50,811.67)},{vPos = Vector3.New(1257.10,-800.50,823.85)}, {vPos = Vector3.New(1242.75,-800.50,823.85)}},
+		{{vPos = Vector3.New(1293.50,-800.50,836.96)},{vPos = Vector3.New(1242.75,-800.50,836.96)}},
+	},
+	West = {
+		{{vPos = Vector3.New(1220.71,-800.50,925.72)},{vPos = Vector3.New(1227.56,-800.50,913.72)}, {vPos = Vector3.New(1234.45,-800.50,925.84)}},
+		{{vPos = Vector3.New(1209.13,-800.50,919.06)},{vPos = Vector3.New(1216.00,-800.50,907.12)}, {vPos = Vector3.New(1202.28,-800.50,907.14)}},
+		{{vPos = Vector3.New(1209.17,-800.50,881.84)},{vPos = Vector3.New(1216.00,-800.50,893.77)}, {vPos = Vector3.New(1202.30,-800.50,893.76)}},
+		{{vPos = Vector3.New(1220.86,-800.50,875.22)},{vPos = Vector3.New(1246.14,-800.50,919.08)}},
+	},
+	East = {
+		{{vPos = Vector3.New(1301.76,-800.50,925.84)},{vPos = Vector3.New(1308.61,-800.50,914.05)}, {vPos = Vector3.New(1315.38,-800.50,925.85)}},
+		{{vPos = Vector3.New(1327.09,-800.50,919.15)},{vPos = Vector3.New(1320.18,-800.50,907.40)}, {vPos = Vector3.New(1333.88,-800.50,907.40)}},
+		{{vPos = Vector3.New(1333.91,-800.50,893.93)},{vPos = Vector3.New(1320.12,-800.50,893.86)}, {vPos = Vector3.New(1327.08,-800.50,882.00)}},
+		{{vPos = Vector3.New(1315.47,-800.50,875.31)},{vPos = Vector3.New(1290.16,-800.50,919.29)}},
+	}
+}
 
-local function augmentorsInit(unit)
-
-	-- Cache health bars for color change
-	tBars[unit:GetId()] = ReStrat.tHealth[unit:GetId()].bar:FindChild("progressBar")
-
-	if not ReStrat.tEncounterVariables.bStarted then
-		ReStrat:OnDatachron("ENGAGING TECHNOPHAGE TRASMISSION", function() -- sic
-			ReStrat:createAlert("Expulsion", 12, nil, ReStrat.color.red)
-		end)
-		
-		--ReStrat:repeatAlert({strLabel = "Radiation Bath", fDelay = 15, fRepeat = 30, strColor = ReStrat.color.green})
-		
-		local function Corrupted(unit) 
-			tBars[unit:GetId()]:SetBarColor(ReStrat.color.purple)
-		end
-		
-		local function Uncorrupted(unit)
-			tBars[unit:GetId()]:SetBarColor(ReStrat.color.orange)
-		end
-		
-		ReStrat:createAuraTrigger(nil, "Compromised Circuitry", Corrupted, Uncorrupted)			
-		
-		ReStrat:createPinFromAura("Strain Incubation")
-				
-		-- Interrupt timer for hardmode laser
-		ReStrat:repeatAlert({strLabel = "Laser Interrupt", fDelay = 8, fRepeat = 14, strColor = ReStrat.color.yellow, fCallback = function()
-			ReStrat:createPop("Interrupt !")
-			Sound.Play(Sound.PlayUIQueuePopsPvP)
-		end})
-		 
-		ReStrat.tEncounterVariables.bStarted = true
+local function Corrupted(unit)
+	local tUnit = tUnits[unit:GetId()] 
+	tUnit.bar:SetBarColor(ReStrat.color.purple)
+	tUnit.paths = {}
+	for _,path in pairs(tSafeZones[tUnit.name]) do
+		tUnit.paths[#tUnit.paths+1] = DrawLib:Path(path)
 	end
-	
-	local pos = unit:GetPosition()
-	local name = pos.z < 875 and "North" or (pos.x < 1268 and "West" or "East")
-	ReStrat:trackHealth(unit, ReStrat.color[name == "North" and "purple" or "orange"], name)
-	
-	local function push()
-		ReStrat:createPop("PUSH SOON !")
-		Sound.PlayFile("Sound\\quack.wav")
-	end
-	
-	-- Pulls below 20 and 60
-	ReStrat:onHealth(unit, 62, push)
-	ReStrat:onHealth(unit, 22, push)
 end
 
-ReStrat.tEncounters["Prime Phage Distributor"]    = { fInitFunction = augmentorsInit }
-ReStrat.tEncounters["Prime Evolutionary Operant"] = { fInitFunction = augmentorsInit }
+local function Uncorrupted(unit)
+	local tUnit = tUnits[unit:GetId()] 
+	tUnit.bar:SetBarColor(ReStrat.color.orange)
+	for _,path in pairs(tUnit.paths) do
+		DrawLib:Destroy(path)
+	end
+	tUnit.paths = {}
+end
+
+local function encounterInit()
+	
+	for _,tUnit in pairs(tUnits) do
+		for _,path in pairs(tUnit.paths) do
+			DrawLib:Destroy(path)
+		end
+	end
+	
+	tUnits = {}
+	
+	ReStrat:createPinFromAura("Strain Incubation")
+	
+	ReStrat:OnDatachron("ENGAGING TECHNOPHAGE TRASMISSION", function() -- sic
+		ReStrat:createAlert("Expulsion", 12, nil, ReStrat.color.red)
+	end)
+	
+	--ReStrat:repeatAlert({strLabel = "Radiation Bath", fDelay = 15, fRepeat = 30, strColor = ReStrat.color.green})
+	
+	ReStrat:createAuraTrigger(nil, "Compromised Circuitry", Corrupted)
+	ReStrat:createCastTrigger(nil, "Expulsion", Uncorrupted)
+						
+	-- Interrupt timer for hardmode laser
+	ReStrat:repeatAlert({strLabel = "Laser Interrupt", fDelay = 8, fRepeat = 14, strColor = ReStrat.color.yellow, fCallback = function()
+		ReStrat:createPop("Interrupt !", nil, Sound.PlayUIQueuePopsPvP)
+	end})
+	
+end
+
+local function augmentorInit(unit)
+
+	if not ReStrat.tEncounterVariables.bStarted then
+		encounterInit()
+		ReStrat.tEncounterVariables.bStarted = true
+	end
+
+	local pos = unit:GetPosition()
+	local name = pos.z < 875 and "North" or (pos.x < 1268 and "West" or "East")
+	ReStrat:trackHealth(unit, ReStrat.color.orange, name)
+	
+	-- Cache name and health bars
+	tUnits[unit:GetId()] = { name = name, bar = ReStrat.tHealth[unit:GetId()].bar:FindChild("progressBar") }
+
+	if name == "North" then Corrupted(unit) end
+
+	-- Pulls below 20 and 60
+	local function push() ReStrat:createPop("PUSH SOON !", nil, "Sound\\quack.wav") end
+	ReStrat:onHealth(unit, 62, push)
+	ReStrat:onHealth(unit, 22, push)
+	
+end
+
+ReStrat.tEncounters["Prime Phage Distributor"]    = { fInitFunction = augmentorInit }
+ReStrat.tEncounters["Prime Evolutionary Operant"] = { fInitFunction = augmentorInit }
